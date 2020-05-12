b4dec2b9e31f4a39e1b5cee59e7c9fc08ea1a0e2
Blananas2 <blananas2@gmail.com>
2020-05-12 01:24:21 -0400

    did thing

---
+++ Assets/templateScript.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using KModkit;
+
+public class templateScript : MonoBehaviour {
+
+    public KMBombInfo Bomb;
+    public KMAudio Audio;
+
+    //Logging
+    static int moduleIdCounter = 1;
+    int moduleId;
+    private bool moduleSolved;
+
+    void Awake () {
+        moduleId = moduleIdCounter++;
+        /*/
+        foreach (KMSelectable object in keypad) {
+            KMSelectable pressedObject = object;
+            object.OnInteract += delegate () { keypadPress(pressedObject); return false; };
+        }
+        /*/
+
+        //button.OnInteract += delegate () { PressButton(); return false; };
+        
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
